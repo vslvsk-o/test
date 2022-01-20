ebc1a6a568a7706f85a7554895a24d6b3f2a6cfd
vslvsk-o <vslvsk@inbox.ru>
2022-01-20 15:48:50 +0300

    Init

--- src/main/java/vslvsk/lesson1/FirstApp.java
+++ src/main/java/vslvsk/lesson1/FirstApp.java
@@ -4,10 +4,12 @@ public class FirstApp {
     //поля
     //методы
     public static void main (String [] args) {
-        int a = 10;
-        int b = 5;
+        int a = 115;
+        int b = 100;
         int c = a + b;
         System.out.println("c="+c);
+        System.out.println("Изменения прошли успешно");
     }
     //классы
+    //изменения
 }
